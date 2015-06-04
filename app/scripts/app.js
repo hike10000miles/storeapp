40673dd4470ebe6d7a339a359fc3daf6444cb4fd
hike10000miles <chenshen4009@gmail.com>
2015-06-04 11:10:06 -0400

    more feature in the app.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,27 +6,42 @@
   var watches =[
     {
   	  brand: 'Omega',
-  	  modle: 'Speedmaster',
+  	  modle: 'Speedmaster professional',
   	  price: 6000,
   	  description: 'A true classic, the first ever watch went to the space!',
-  	  canPurchase: false,
+  	  canPurchase: true,
   	  soldOut: false,
+      images: [
+        {
+          full:'../images/Speedmaster.jpg'
+        }
+      ],
     },
     {
       brand: 'Rolex',
   	  modle: 'Submariner',
   	  price: 8000,
   	  description: "The first Bond's watch, a classic explorer's watch.",
-  	  canPurchase: false,
+  	  canPurchase: true,
   	  soldOut: false,
+      images: [
+        {
+          full:'../images/Rolex-Submariner.jpg'
+        }
+      ]
     },
     {
-      brand: 'Rolex',
-  	  modle: 'Submariner',
-  	  price: 8000,
-  	  description: "The first Bond's watch, a classic explorer's watch.",
-  	  canPurchase: false,
+      brand: 'Zenith',
+  	  modle: 'El Primero',
+  	  price: 7600,
+  	  description: "The finest of the chronograph watches.",
+  	  canPurchase: true,
   	  soldOut: false,
+      images: [
+        {
+          full:'../images/Zenith El Primero.jpg'
+        }
+      ]      
     }
   ];
 
